ff9f353ce3c763394004820f313e6ba6fadd0921
Mike <veldsinkie.mv@gmail.com>
2024-01-11 02:00:41 +0100

    🤓 100% test coverage

--- Mipa.Tests/ParserTests.cs
+++ Mipa.Tests/ParserTests.cs
@@ -1,7 +1,31 @@
 namespace Mipa.Tests;
 
+public record UnparsableType : IParsable<UnparsableType>
+{
+    public static UnparsableType Parse(string s, IFormatProvider? provider)
+    {
+        throw new Exception();
+    }
+
+    public static bool TryParse(string value, IFormatProvider? formatProvider, out UnparsableType result)
+    {
+        result = new UnparsableType();
+        return false;
+    }
+}
+
 public class Tests
 {
+    [Test]
+    public void EmptyText()
+    {
+        var parser = new MipaParser();
+        var result = parser.Parse("");
+        
+        Assert.That(result.Content, Is.EqualTo(""));
+        Assert.That(result.Arguments, Is.Empty);
+    }
+    
     [Test]
     public void BaseText()
     {
@@ -45,5 +69,22 @@ public class Tests
         Assert.That(result.Arguments, Has.Count.EqualTo(2));
         Assert.That(result.GetArgument("key"), Is.EqualTo("value"));
         Assert.That(result.GetArgument("key2"), Is.EqualTo("this is value2"));
+    }
+
+    [Test]
+    public void ParseArguments()
+    {
+        var parser = new MipaParser();
+        var result = parser.Parse("date:08/08/2023 hello world key:23");
+
+        Assert.That(result.Content, Is.EqualTo("hello world"));
+        Assert.That(result.Arguments, Has.Count.EqualTo(2));
+        Assert.That(result.GetArgument<DateTime>("date"), Is.TypeOf<DateTime>());
+        Assert.That(result.GetArgument<DateTime>("date"), Is.EqualTo(new DateTime(2023, 8, 8)));
+        Assert.That(result.GetArgument<int>("key"), Is.EqualTo(23));
+        
+        // failure states
+        Assert.That(result.GetArgument<UnparsableType>("date"), Is.Null);
+        Assert.That(result.GetArgument<UnparsableType>("non-existant-key"), Is.Null);
     }
 }
